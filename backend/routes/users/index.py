9c91d840cff3dcf54129d57f3f09db5f800c920d
Joseph Banks <joseph@josephbanks.me>
2020-03-05 08:54:23 +0000

    Set up sessionmaker and add a basic database querying route

--- backend/routes/users/index.py
+++ backend/routes/users/index.py
@@ -1,6 +1,9 @@
 """Index page for the User API."""
+from backend.database import Session
 from backend.route import Route
+from backend.models import User
 
+from flask import jsonify
 
 class UserIndex(Route):
     """Index class for the User API."""
@@ -10,4 +13,13 @@ class UserIndex(Route):
 
     def get(self: "UserIndex") -> str:
         """GET request to the User index."""
-        return "Index for User API"
+        sess = Session()
+
+        data = []
+
+        for instance in sess.query(User).order_by(User.id):
+            d = instance.__dict__
+            d.pop("_sa_instance_state")
+            data.append(d)
+
+        return jsonify(data)
