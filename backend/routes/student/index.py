083df10adefcdb0cff911026adc5a613b8c08625
Joseph Banks <joseph@josephbanks.me>
2020-06-18 04:00:06 +0100

    Add a task list for students

--- backend/routes/student/index.py
+++ backend/routes/student/index.py
@@ -1,10 +1,51 @@
 """A portal for allowing students to see their classes."""
 
-from flask import Response, render_template
+from datetime import datetime
+from itertools import groupby
+
+from flask import g, Response, render_template
 
 from backend.models import UserType
 from backend.route import Route
 from backend.utils import authenticated
+
+WEEK_DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
+SUFFIXES = {
+    1: "st",
+    2: "nd",
+    3: "rd",
+    range(4, 21): "th",
+    21: "st",
+    22: "nd",
+    23: "rd",
+    range(24, 31): "th",
+    31: "st",
+}
+
+MONTHS = [
+    None,
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
 
 
 class StudentPortal(Route):
@@ -13,7 +54,34 @@ class StudentPortal(Route):
     name = "index"
     path = "/"
 
+    @staticmethod
+    def format_date(date: datetime) -> str:
+        weekday = WEEK_DAYS[date.weekday()]
+
+        suffix = ""
+
+        for day_or_range, suf in SUFFIXES.items():
+            if isinstance(day_or_range, int):
+                if date.day == day_or_range:
+                    suffix = suf
+            else:
+                if date.day in day_or_range:
+                    suffix = suf
+
+        return f"{weekday}, {date.day}{suffix} {MONTHS[date.month]}, {date.year}"
+
     @authenticated(user_type=UserType.STUDENT)
     def get(self) -> Response:  # skipcq: PYL-R0201
         """Display a portal page to the user."""
-        return render_template("student/index.html")
+        classes = g.user.classes
+
+        tasks = []
+
+        for cm in classes:
+            tasks.extend(cm.cls.tasks)
+
+        tasks_by_date = groupby(tasks, lambda task: self.format_date(task.due_at))
+
+        return render_template(
+            "student/index.html", formatter=self.format_date, tasks=tasks_by_date
+        )
