8a54dc75b5041e62d8e4fd7acc7c7717369afb85
Joseph Banks <joseph@josephbanks.me>
2020-06-22 02:27:31 +0100

    Switch student join to use new database

--- backend/routes/student/join.py
+++ backend/routes/student/join.py
@@ -18,13 +18,13 @@ class StudentJoin(Route):
         """Display a portal page to the user."""
         code = request.form.get("class_code")
 
-        cls = self.sess.query(Class).filter_by(code=code).first()
+        cls = Class.query.filter_by(code=code).first()
 
         if not cls:
             return render_template("student/index.html", error="Could not find class")
 
         class_membership = ClassMembership(user=g.user, cls=cls)
 
-        self.sess.add(class_membership)
-        self.sess.commit()
+        self.app.db.session.add(class_membership)
+        self.app.db.session.commit()
         return redirect(url_for("student.index"))
